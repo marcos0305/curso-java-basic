e1f14d5cd5728b4419931af55ab561d1eaac2efa
Marcos Paulo <carvalho.olveira0305@gmail.com>
2022-12-09 20:13:17 -0300

    Curso de Java 10: Introdução a Variávels

---
+++ src/Erros01.java
@@ -0,0 +1,9 @@
+
+public class Erros01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Word");
+	}
+
+}
---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,47 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		
+		//convenção Java
+		
+		int idade = 22;
+		String nome = "Marcos";
+		String nomeDoMeuGato = "Vader";
+		String = "ano2022";
+		
+		
+		//aceito,mas não utilizado
+		int _idade;
+		int $idade;
+		
+		
+		//não é convenção Java
+		
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomeDoMeuCachorro;
+		
+		idade = 22;
+		
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		
+		
+		//má prática
+		int a = 10;
+		String b = "Marcos";
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
