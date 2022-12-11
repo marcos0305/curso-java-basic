08f57047ce6f0c008d888291524ad993463ea91b
Marcos Paulo <carvalho.olveira0305@gmail.com>
2022-12-11 16:40:09 -0300

    Curso de Java 11: Variáveis - Tipos Primitivos

---
+++ src/com/loiane/cursojava/aula11/VaariaveisBoolean.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula11;
+
+public class VaariaveisBoolean {
+
+	public static void main(String[] args) {
+		
+		boolean verdadeiro = true;
+		
+		boolean falso = false;
+		
+		System.out.println("O valor de verdadeiro é " + verdadeiro);
+		System.out.println("O valor de false é " + falso);
+
+	}
+
+}
---
+++ src/com/loiane/cursojava/aula11/VariaveisPontosFlutuante.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisPontosFlutuante {
+
+	public static void main(String[] args) {
+		
+		double valorPassagem = 2.70;
+		
+		float valorTomate = 4.55f;
+		
+		System.out.println("Valor da passagem = " + valorPassagem);
+		System.out.println("Valor do tomate = " + valorTomate);
+
+	}
+
+}
