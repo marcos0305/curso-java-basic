84b176cda4dfd49e6aec873e588edfabddc342b2
Marcos Paulo <carvalho.olveira0305@gmail.com>
2023-02-02 00:54:22 -0300

    Curso de Java 21: Loop for each

---
+++ src/com/loiane/cursojava/aula21/ForEach.java
@@ -0,0 +1,29 @@
+package com.loiane.cursojava.aula21;
+
+import java.util.Random;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		
+		int [] notas = new int [10];
+		
+		Random random = new Random ();
+		
+		for (int i=0; i<notas.length; i++) {
+			
+			notas[i] = random.nextInt(10);
+						
+			}
+				for (int  i=0;  i<notas.length; i++) {
+					System.out.println(notas[ i]);
+				}
+		
+			System.out.println("Usando For Each");
+			for(int nota : notas) {
+				System.out.println(nota);
+		}
+			
+	}
+
+}
