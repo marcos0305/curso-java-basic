11970c3d6990fed764689d03d1bc328433946194
Marcos Paulo <carvalho.olveira0305@gmail.com>
2023-03-30 13:45:08 -0300

    Curso de Java 34 - Orientação a Objetos: Variáveis e metodos estáticos (modificador static)

---
+++ src/com/loiane/cursojava/aula34/CalculadoraTeste.java
@@ -0,0 +1,16 @@
+package com.loiane.cursojava.aula34;
+
+public class CalculadoraTeste {
+	
+	public static void main(String [] args) {
+		
+		//MinhaCalculadora calc  = new MinhaCalculadora();
+		
+		//calc.soma(2, 3);
+		
+		MinhaCalculadora.soma(02, 05);
+		MinhaCalculadora.soma(05, 015, 025);
+		
+			}
+	
+}
