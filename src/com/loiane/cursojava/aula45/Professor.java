46f4845713cc1b104731411ca036f83980e02314
marcos0305 <carvalho.olveira0305@gmail.com>
2023-05-18 16:27:20 -0300

    Curso de Java 45: Orientação a Objetos: Casting e instaceof

---
+++ src/com/loiane/cursojava/aula45/Professor.java
@@ -0,0 +1,35 @@
+package com.loiane.cursojava.aula45;
+
+public class Professor extends Pessoa {
+
+	private String nomeCurso;
+	private double[] salario;
+	
+	
+	public double[] getSalario() {
+		return salario;
+	}
+	public void setSalario(double[] salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	   
+	public double calcularSalarioLiquido() {
+		return 0;
+	}
+	
+	public String obterEtiquetaEndereco() {
+		
+		String s = "Endereco do Professor";
+		s += super.getEndereco();
+		
+		return s;
+		
+	}
+}
+	
